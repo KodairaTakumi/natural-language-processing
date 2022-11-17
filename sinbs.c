98d39cb8ec6de1db46e1415d491b01e565aa852f
Kodaira Takumi <a48565493@icloud.com>
2022-11-17 11:39:38 +0900

    meke ploguramu

---
+++ sinbs.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include<ctype.h>
+  
+  
+  int chek(char ch){
+  	int ans;
+  	if(isprint(ch)){
+  		ans = 1;
+  	} else if(iscntrl(ch)){
+  		ans = 1;
+  	}else {
+  		ans = 0;
+  	}
+  	return ans;
+  }
+int main(void){
+  
+  char ch;
+  FILE *fp;
+ 
+  fp = fopen("file.txt", "r");
+  
+  while((ch = fgetc(fp)) != EOF){
+  	if(chek(ch)){
+  		printf("e");
+  	} else {
+  		printf("\n");
+  	}
+  }
+  
+  fclose(fp);
+ 
+  return 0;
+}
+
