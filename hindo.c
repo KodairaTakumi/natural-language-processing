6f962186bd444683f82be0cb9c6ed95360d15aca
Kodaira Takumi <a48565493@icloud.com>
2022-11-29 20:52:57 +0900

    onetxt smole

--- hindo.c
+++ hindo.c
@@ -41,7 +41,7 @@ int main(){
 	FILE *co;
 	FILE *ot;
 	int dt[29] = {};
-	char ab[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char ab[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	char tx[999999] = {};
 	int eng,i,nb,j=0;
 	double tt;
